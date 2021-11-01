84738dd743cadec3c24c7d4c56bf9acc71e9b8c6
patricia milane <patriciamilane@hotmail.com>
2021-11-01 01:05:33 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+ 
+ function carregar(){
+
+    var msg =  document.getElementById('msg')
+    var img = document.getElementById('foto')
+    var data = new Date()
+    var hora = data.getHours()
+    var msg2 = document.getElementById('msg2')
+    // var hora = 19
+
+    msg.innerHTML = `Agora são ${hora} horas. `
+
+    if (hora >=0 && hora < 12){
+
+        img.src='3.png'
+        document.body.style.background = 'lightblue'
+        msg2.innerHTML = `Bom dia!`
+
+    } else if (hora >= 12 && hora < 18){
+
+        img.src='2.png'
+        document.body.style.background = '#4682B4'
+        msg2.innerHTML += `Boa tarde!`
+
+    
+    } else {
+        img.src='1.png'
+        document.body.style.background = '#191970'
+        msg2.innerHTML += `Boa noite!`
+
+
+    }
+
+ }
+ 
